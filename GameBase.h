11f8467eebb9be5848a6bfbe036c324357da9e4a
Mac Macoy <mamacoy907@yahoo.com>
2016-11-12 13:24:32 -0500

    upload GameBase.h

---
+++ GameBase.h
@@ -0,0 +1,51 @@
+//#include "tiles.h"
+//#include "board.h"
+//#include "players.h"
+#pragma once
+#include <iostream>
+
+
+#include "board.h"
+#include "player.h"
+#include "Tile.h"
+
+
+class Gamebase
+{
+public:
+	Gamebase();
+//	Gamebase(Board* b, Player* p1, Player* p2);
+	~Gamebase();
+
+	bool Run();
+	
+
+	bool start;
+	bool exit;
+
+	bool end;
+
+private:
+	//PlayerLogics
+	//AddTiles
+	//etc
+
+	bool isGameEnded();
+	
+	Board* board;
+	
+	int turn;
+	bool turnOver;
+	Tile* tile;
+	//Player* player1;
+	//Player* player2;
+	
+
+
+
+
+
+
+
+
+};
