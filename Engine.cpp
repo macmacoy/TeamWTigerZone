5bf63002a1872cf6689a976a1c4795ae8d4df5a7
Mac Macoy <mamacoy907@yahoo.com>
2016-11-23 21:33:32 -0600

    I think engine is fixed for testing

--- Engine.cpp
+++ Engine.cpp
@@ -5,8 +5,8 @@
 Engine::Engine(int localTest)
 {
 	//Initialize the game engine
-	MakeDecksTest(); // for testing
-	Gamebase* game = new Gamebase(P1_deck, P2_deck);
+	MakedsTest(); // for testing
+	Gamebase* game = new Gamebase(deck);
 	while (!game->RunTest()){}
 	cout << "GAME OVER\n\n";
 }
@@ -14,7 +14,7 @@ Engine::Engine(int localTest)
 // server game engine
 Engine::Engine(std::vector<std::string>& tiles) // some unkown input
 {
-	MakeDecks(tiles);
+	Makeds(tiles);
 	// AI is player 1
 	game1 = new Gamebase(deck);
 	// AI is player 2
@@ -29,100 +29,94 @@ Engine::~Engine()
 // for testing
 void Engine::MakeDecksTest(){
 
-	Tile* deck[76];
+	Tile* d[76];
 
-	deck[0] 	= new Tile(1,1,1,1,1,0);
+	d[0] 	= new Tile(1,1,1,1,1,0);
 	//Type 2
-	deck[1] 	= new Tile(1,1,1,1,4,0);
-	deck[2] 	= new Tile(1,1,1,1,4,0);
-	deck[3] 	= new Tile(1,1,1,1,4,0);
-	deck[4] 	= new Tile(1,1,1,1,4,0);
+	d[1] 	= new Tile(1,1,1,1,4,0);
+	d[2] 	= new Tile(1,1,1,1,4,0);
+	d[3] 	= new Tile(1,1,1,1,4,0);
+	d[4] 	= new Tile(1,1,1,1,4,0);
 	//Type 3
-	deck[5]		= new Tile(1,1,3,1,4,0);
-	deck[6]		= new Tile(1,1,3,1,4,0);
+	d[5]		= new Tile(1,1,3,1,4,0);
+	d[6]		= new Tile(1,1,3,1,4,0);
 	//Type 4
-	deck[7] 	= new Tile(3,3,3,3,3,0);
+	d[7] 	= new Tile(3,3,3,3,3,0);
 	//Type 5
-	deck[8] 	= new Tile(3,1,3,1,5,0);
-	deck[9] 	= new Tile(3,1,3,1,5,0);
-	deck[10] 	= new Tile(3,1,3,1,5,0);
-	deck[11] 	= new Tile(3,1,3,1,5,0);
-	deck[12] 	= new Tile(3,1,3,1,5,0);
-	deck[13] 	= new Tile(3,1,3,1,5,0);
-	deck[14] 	= new Tile(3,1,3,1,5,0);
-	deck[15] 	= new Tile(3,1,3,1,5,0);
+	d[8] 	= new Tile(3,1,3,1,5,0);
+	d[9] 	= new Tile(3,1,3,1,5,0);
+	d[10] 	= new Tile(3,1,3,1,5,0);
+	d[11] 	= new Tile(3,1,3,1,5,0);
+	d[12] 	= new Tile(3,1,3,1,5,0);
+	d[13] 	= new Tile(3,1,3,1,5,0);
+	d[14] 	= new Tile(3,1,3,1,5,0);
+	d[15] 	= new Tile(3,1,3,1,5,0);
 	//Type 6
-	deck[16] 	= new Tile(3,1,1,3,5,0);
-	deck[17] 	= new Tile(3,1,1,3,5,0);	
-	deck[18] 	= new Tile(3,1,1,3,5,0);
-	deck[19] 	= new Tile(3,1,1,3,5,0);
-	deck[20] 	= new Tile(3,1,1,3,5,0);
-	deck[21] 	= new Tile(3,1,1,3,5,0);
-	deck[22] 	= new Tile(3,1,1,3,5,0);
-	deck[23] 	= new Tile(3,1,1,3,5,0);
-	deck[24] 	= new Tile(3,1,1,3,5,0);
-	deck[25] 	= new Tile(3,1,3,3,3,0);
-	deck[26] 	= new Tile(3,1,3,3,3,0);
-	deck[27] 	= new Tile(3,1,3,3,3,0);
-	deck[28] 	= new Tile(3,1,3,3,3,0);
-	deck[29] 	= new Tile(2,2,2,2,2,0);
-	deck[30] 	= new Tile(1,2,2,2,2,0);
-	deck[31] 	= new Tile(1,2,2,2,2,0);
-	deck[32] 	= new Tile(1,2,2,2,2,0);
-	deck[33] 	= new Tile(1,2,2,2,2,0);
-	deck[34] 	= new Tile(2,2,1,1,2,0);
-	deck[35] 	= new Tile(2,2,1,1,2,0);
-	deck[36] 	= new Tile(2,2,1,1,2,0);
-	deck[37] 	= new Tile(2,2,1,1,2,0);
-	deck[38] 	= new Tile(2,2,1,1,2,0);
-	deck[39] 	= new Tile(1,2,1,2,2,0);
-	deck[40] 	= new Tile(1,2,1,2,2,0);
-	deck[41] 	= new Tile(1,2,1,2,2,0);
-	deck[42] 	= new Tile(2,1,2,1,1,0);
-	deck[43] 	= new Tile(2,1,2,1,1,0);
-	deck[44] 	= new Tile(2,1,2,1,1,0);
-	deck[45] 	= new Tile(2,1,1,1,1,0);
-	deck[46] 	= new Tile(2,1,1,1,1,0);
-	deck[47] 	= new Tile(2,1,1,1,1,0);
-	deck[48] 	= new Tile(2,1,1,1,1,0);
-	deck[49] 	= new Tile(2,1,1,1,1,0);
-	deck[50] 	= new Tile(1,2,2,1,1,0);
-	deck[51] 	= new Tile(1,2,2,1,1,0);
-	deck[52] 	= new Tile(3,2,1,3,5,0);
-	deck[53] 	= new Tile(3,2,1,3,5,1);
-	deck[54] 	= new Tile(3,2,1,3,5,1);
-	deck[55] 	= new Tile(1,2,3,3,5,0);
-	deck[56] 	= new Tile(1,2,3,3,5,1);
-	deck[57] 	= new Tile(1,2,3,3,5,1);
-	deck[58] 	= new Tile(3,2,3,1,5,0);
-	deck[59] 	= new Tile(3,2,3,1,5,0);
-	deck[60] 	= new Tile(3,2,3,1,5,1);
-	deck[61] 	= new Tile(3,2,3,1,5,1);
-	deck[62] 	= new Tile(3,2,2,2,2,0);
-	deck[63] 	= new Tile(3,2,2,2,2,0);
-	deck[64] 	= new Tile(3,2,2,2,2,0);
-	deck[65] 	= new Tile(3,2,3,3,3,0);
-	deck[66] 	= new Tile(3,2,3,3,3,1);
-	deck[67] 	= new Tile(3,2,3,3,3,1);
-	deck[68] 	= new Tile(3,2,2,3,5,0);
-	deck[69] 	= new Tile(3,2,2,3,5,0);
-	deck[70] 	= new Tile(3,2,2,3,5,0);
-	deck[71] 	= new Tile(3,2,2,3,5,1);
-	deck[72] 	= new Tile(3,2,2,3,5,1);
-	deck[73] 	= new Tile(2,1,3,1,1,0);
-	deck[74] 	= new Tile(2,1,3,1,1,1);
-	deck[75] 	= new Tile(2,1,3,1,1,1);
+	d[16] 	= new Tile(3,1,1,3,5,0);
+	d[17] 	= new Tile(3,1,1,3,5,0);	
+	d[18] 	= new Tile(3,1,1,3,5,0);
+	d[19] 	= new Tile(3,1,1,3,5,0);
+	d[20] 	= new Tile(3,1,1,3,5,0);
+	d[21] 	= new Tile(3,1,1,3,5,0);
+	d[22] 	= new Tile(3,1,1,3,5,0);
+	d[23] 	= new Tile(3,1,1,3,5,0);
+	d[24] 	= new Tile(3,1,1,3,5,0);
+	d[25] 	= new Tile(3,1,3,3,3,0);
+	d[26] 	= new Tile(3,1,3,3,3,0);
+	d[27] 	= new Tile(3,1,3,3,3,0);
+	d[28] 	= new Tile(3,1,3,3,3,0);
+	d[29] 	= new Tile(2,2,2,2,2,0);
+	d[30] 	= new Tile(1,2,2,2,2,0);
+	d[31] 	= new Tile(1,2,2,2,2,0);
+	d[32] 	= new Tile(1,2,2,2,2,0);
+	d[33] 	= new Tile(1,2,2,2,2,0);
+	d[34] 	= new Tile(2,2,1,1,2,0);
+	d[35] 	= new Tile(2,2,1,1,2,0);
+	d[36] 	= new Tile(2,2,1,1,2,0);
+	d[37] 	= new Tile(2,2,1,1,2,0);
+	d[38] 	= new Tile(2,2,1,1,2,0);
+	d[39] 	= new Tile(1,2,1,2,2,0);
+	d[40] 	= new Tile(1,2,1,2,2,0);
+	d[41] 	= new Tile(1,2,1,2,2,0);
+	d[42] 	= new Tile(2,1,2,1,1,0);
+	d[43] 	= new Tile(2,1,2,1,1,0);
+	d[44] 	= new Tile(2,1,2,1,1,0);
+	d[45] 	= new Tile(2,1,1,1,1,0);
+	d[46] 	= new Tile(2,1,1,1,1,0);
+	d[47] 	= new Tile(2,1,1,1,1,0);
+	d[48] 	= new Tile(2,1,1,1,1,0);
+	d[49] 	= new Tile(2,1,1,1,1,0);
+	d[50] 	= new Tile(1,2,2,1,1,0);
+	d[51] 	= new Tile(1,2,2,1,1,0);
+	d[52] 	= new Tile(3,2,1,3,5,0);
+	d[53] 	= new Tile(3,2,1,3,5,1);
+	d[54] 	= new Tile(3,2,1,3,5,1);
+	d[55] 	= new Tile(1,2,3,3,5,0);
+	d[56] 	= new Tile(1,2,3,3,5,1);
+	d[57] 	= new Tile(1,2,3,3,5,1);
+	d[58] 	= new Tile(3,2,3,1,5,0);
+	d[59] 	= new Tile(3,2,3,1,5,0);
+	d[60] 	= new Tile(3,2,3,1,5,1);
+	d[61] 	= new Tile(3,2,3,1,5,1);
+	d[62] 	= new Tile(3,2,2,2,2,0);
+	d[63] 	= new Tile(3,2,2,2,2,0);
+	d[64] 	= new Tile(3,2,2,2,2,0);
+	d[65] 	= new Tile(3,2,3,3,3,0);
+	d[66] 	= new Tile(3,2,3,3,3,1);
+	d[67] 	= new Tile(3,2,3,3,3,1);
+	d[68] 	= new Tile(3,2,2,3,5,0);
+	d[69] 	= new Tile(3,2,2,3,5,0);
+	d[70] 	= new Tile(3,2,2,3,5,0);
+	d[71] 	= new Tile(3,2,2,3,5,1);
+	d[72] 	= new Tile(3,2,2,3,5,1);
+	d[73] 	= new Tile(2,1,3,1,1,0);
+	d[74] 	= new Tile(2,1,3,1,1,1);
+	d[75] 	= new Tile(2,1,3,1,1,1);
 
-	// assign every other tile to decks 1 & 2
-	int index = 0;
-	for(int i=2; i<78; i++){
-		if(i%2 == 0)
-			P1_deck.push_back(*deck[i-2]);
-		if(i%2 == 1){
-			P2_deck.push_back(*deck[i-2]);
-			index++;
-		}
+	for(int i=0; i<76; i++){
+		deck.push(d[i]);
 	}
+
 }
 
 // translate string values to our tiles
