fcdb10566727f87171a03bf24015a2b9bb4a74f6
chetaylo844 <churro555sastre@ufl.edu>
2016-12-02 09:01:52 -0800

    Add files via upload

---
+++ TestFiles/TestCompletedDen.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "src/Board.cpp"
+
+int main(int argc, char *argv[]){
+
+	Board* b = new Board();
+	b->PlaceTile(new Tile(2,1,2,1,3,0), 71, 70);
+	b->PlaceTile(new Tile(1,1,1,1,4,0), 72, 70);
+	b->PlaceTile(new Tile(1,1,2,3,3,0), 72, 71);
+	b->PlaceTile(new Tile(2,2,2,1,5,0), 73, 70);
+	b->PlaceTile(new Tile(2,2,1,1,5,0), 72, 69);
+	b->DisplayBoard();
+	int result = b->CheckCompletedDen(72,70);
+	cout << result << "\n";
+}
